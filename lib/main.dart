4dfa648bcba2fb376362d95e067c926aad3e5e40
Dino Gaston  Nunez Pintado <dinopintado.sl015@academico.ifsul.edu.br>
2026-03-04 21:04:50 -0300

    column, row, container, elevatedbutton, center

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Pagina1());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Pagina1 extends StatelessWidget {
+  const Pagina1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("App Tests"),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+
+        body: Container(
+          margin: EdgeInsets.all(50),
+          decoration: BoxDecoration(border: Border.all()),
+          padding: EdgeInsets.all(50),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Contenido',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+          
+                Text(
+                  'Contenido 2',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+          
+                ElevatedButton(onPressed: () {exit(0);}, child: Text('Cerrar')),
+          
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(width: 10), 
+                    Text('  Fila 1  '),
+                    Text('  Fila 2  '),
+                    Text('  Fila 3  '),]
+                ),
+          
+                
+              ],
+            ),
+          ),
         ),
       ),
     );
